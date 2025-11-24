85b9feaabdf90904351412baa840ce5e7ce324d7
SHIVA VISHWAKARMA <raw66010@gmail.com>
2025-11-24 20:44:36 +0530

    add vercel.json

--- Frontend/MyAdmin/vite.config.js
+++ Frontend/MyAdmin/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
