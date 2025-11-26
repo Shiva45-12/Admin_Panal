f5a4b5dd9d675de7ce907ea70910eb9f4edf14eb
SHIVA VISHWAKARMA <raw66010@gmail.com>
2025-11-26 19:11:18 +0530

    cards changes

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/adminpanel");
+    await mongoose.connect(process.env.MONGODB_URI) 
     console.log("MongoDB Connected successfully");
   } catch (error) {
     console.error("Database connection failed:", error.message);
